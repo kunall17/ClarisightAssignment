3cd5813fee37c374f054367f3ad468814363bf07
Kunal Gupta <kunall.gupta17@gmail.com>
2018-09-25 00:08:06 +0530

    tests: Fix jest test's

--- js/__tests__/binarytree.test.js
+++ js/__tests__/binarytree.test.js
@@ -7,34 +7,42 @@ const {
 } = require('../BinaryTree');
 
 var bt = new BinaryTree();
-bt.push(1);
-bt.push(2);
-bt.push(3);
-bt.push(4);
-bt.push(5);
+bt.push(1, "a");
+bt.push(2, "b");
+bt.push(3, "c");
+bt.push(4, "d");
+bt.push(5, "e");
 
 describe('Test traversals' , () => {
   test('Test inorder', () => {
     var x = [];
-    inorder(bt.root, x);
+    var y = [];
+    inorder(bt.root, x, y);
     expect(x).toEqual([4, 2, 5, 1, 3]);
+    expect(y).toEqual(["d", "b", "e", "a", "c"]);
   });
 
   test('Test preorder', () => {
     var x = [];
-    preorder(bt.root, x);
+    var y = [];
+    preorder(bt.root, x, y);
     expect(x).toEqual([1, 2, 4, 5, 3]);
+    expect(y).toEqual(["a", "b", "d", "e", "c"]);
   });
   
   test('Test postorder', () => {
     var x = [];
-    postorder(bt.root, x);
+    var y = [];
+    postorder(bt.root, x, y);
     expect(x).toEqual([4, 5, 2, 3, 1]);
+    expect(y).toEqual(["d", "e", "b", "c", "a"]);
   });
 
   test('Test bfs', () => {
     var x = [];
-    bfs(bt.root, x);
+    var y = [];
+    bfs(bt.root, x, y);
     expect(x).toEqual([1, 2, 3, 4, 5]);
+    expect(y).toEqual(["a", "b", "c", "d", "e"]);
   });
 });
